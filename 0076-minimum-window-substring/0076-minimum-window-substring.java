6695fcb9b2869a2dc882d74f40362298a65189b3
Soham Bangar <sohambangar188@gmail.com>
2026-08-27 02:04:35 +0530

    Time: 118 ms (10.93%), Space: 47.7 MB (23.45%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,44 +1,34 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-
-        HashMap<Character, Integer> need = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            need.put(c, need.getOrDefault(c, 0) + 1);
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
         }
-
-        HashMap<Character, Integer> window = new HashMap<>();
-        int required = need.size();   // distinct chars needed with correct freq
-        int formed = 0;                // distinct chars currently satisfied
-
-        int left = 0;
+        int left = 0, count = t.length();
         int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
+        String ans = "";
 
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            window.put(c, window.getOrDefault(c, 0) + 1);
+        for(int right = 0; right<s.length(); right++){
+            char ch = s.charAt(right);
 
-            if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
-                formed++;
+            if(map.containsKey(ch)){
+                if(map.get(ch)>0) count--;
+                map.put(ch, map.get(ch) - 1);
             }
-
-            // shrink while window is valid
-            while (formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    minStart = left;
+            while(count == 0){
+                if(right-left+1<minLen){
+                    minLen = right-left+1;
+                    ans = s.substring(left, right+1); 
                 }
-
                 char leftChar = s.charAt(left);
-                window.put(leftChar, window.get(leftChar) - 1);
-                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
-                    formed--;
+                if(map.containsKey(leftChar)){
+                    map.put(leftChar, map.get(leftChar)+1);
+                    if(map.get(leftChar)>0) count ++;
                 }
-                left++;
+                left ++;
             }
-        }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        }
+        return ans;
     }
 }
