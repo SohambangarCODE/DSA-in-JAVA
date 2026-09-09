403e774dab2d85939b4a276a6e8d3e84f4a4ce3e
Soham Bangar <sohambangar188@gmail.com>
2026-09-09 23:59:29 +0530

    Time: 28 ms (96.59%), Space: 59.4 MB (23.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
-
         for(int i = 0; i<nums.length-2; i++){
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
@@ -25,6 +24,7 @@ class Solution {
                     k--;
                 }
             }
+            
         }
         return list;
     }
