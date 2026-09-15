3306748c620766ed2fa9347aea5c00a589494fec
Soham Bangar <sohambangar188@gmail.com>
2026-09-15 21:55:02 +0530

    Time: 30 ms (89.36%), Space: 104.5 MB (7.99%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,40 @@
+class MinStack {
+
+    private Node head;
+
+    public MinStack() {
+        head = null;
+    }
+
+    public void push(int value) {
+        if (head == null) {
+            head = new Node(value, value, null);
+        } else {
+            head = new Node(value, Math.min(value, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private static class Node {
+        int val;
+        int min;
+        Node next;
+
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
